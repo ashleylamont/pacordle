55fd7f3e740a58c13335b315e1069ed49ff32aea
Ashley Lamont <benjamin.m.lamont@gmail.com>
2022-02-19 01:50:17 +1100

    Welcome message and credits.

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,16 @@ import 'animate.css';
 import MenuBar from './components/MenuBar';
 import Grid from './components/Grid';
 import Game from './Game';
+import Overlay from './components/Overlay';
 
 const app = document.querySelector<HTMLDivElement>('#app')!;
 new MenuBar(app).setTitle('PACordle');
 const game = new Game();
 const grid = new Grid(app, game);
+const overlay = new Overlay(app, 'Welcome to PACordle!', 'Did you ever wish Wordle '
+    + 'could be worse? Well now it is! PACordle is like Wordle, but all the answers are degree '
+    + 'codes from ANU Programs and Courses, so it\'s stupid hard to guess 😆. Good luck!');
+console.log(overlay);
 
 // Add fireworks container.
 const fireworksContainer = document.createElement('div');
